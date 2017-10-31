93e8ce5f152a4b3bcf811badd6e35c02e5807f62
Store <qxc122@126.com>
2017-10-31 16:36:57 +0800

    add QCC_Base.pods file

---
+++ QCC_Base/QCC_BaseCell.h
@@ -0,0 +1,13 @@
+//
+//  QCC_BaseCell.h
+//  QCCViewRoundRound
+//
+//  Created by Store on 2017/10/31.
+//  Copyright © 2017年 qxc122@126.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCC_BaseCell : UITableViewCell
+
+@end
---
+++ QCC_Base/QCC_BaseCell.m
@@ -0,0 +1,24 @@
+//
+//  QCC_BaseCell.m
+//  QCCViewRoundRound
+//
+//  Created by Store on 2017/10/31.
+//  Copyright © 2017年 qxc122@126.com. All rights reserved.
+//
+
+#import "QCC_BaseCell.h"
+
+@implementation QCC_BaseCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
